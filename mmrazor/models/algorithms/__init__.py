aa96632096ef8fa0ef16ebe6181d1471c5792600
pppppM <gjf_mail@126.com>
2022-04-23 21:53:41 +0800

    remove algorithms who need refactor

--- mmrazor/models/algorithms/__init__.py
+++ mmrazor/models/algorithms/__init__.py
@@ -1,13 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .align_method_kd import AlignMethodDistill
-from .autoslim import AutoSlim
 from .bignas import BigNAS
-from .darts import Darts
-from .detnas import DetNAS
 from .general_distill import GeneralDistill
-from .spos import SPOS
 
 __all__ = [
-    'AutoSlim', 'AlignMethodDistill', 'Darts', 'SPOS', 'DetNAS',
     'GeneralDistill', 'BigNAS'
 ]
