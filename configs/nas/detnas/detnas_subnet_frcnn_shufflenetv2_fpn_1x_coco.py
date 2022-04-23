268517eeb2e7a6102fee536c8eaa5a3ed29cc174
pppppM <gjf_mail@126.com>
2022-04-23 21:51:21 +0800

    remove detnas

--- configs/nas/detnas/detnas_subnet_frcnn_shufflenetv2_fpn_1x_coco.py
+++
@@ -1,3 +0,0 @@
-_base_ = ['./detnas_supernet_frcnn_shufflenetv2_fpn_1x_coco.py']
-
-algorithm = dict(retraining=True)
--- configs/nas/detnas/detnas_subnet_shufflenetv2_8xb128_in1k.py
+++
@@ -1,3 +0,0 @@
-_base_ = [
-    '../spos/spos_subnet_shufflenetv2_8xb128_in1k.py',
-]
