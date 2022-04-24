83751032406ed1f12369397be03e824fd5a237c6
pppppM <gjf_mail@126.com>
2022-04-24 13:43:33 +0800

    remove placholder

--- mmrazor/models/architectures/components/backbones/__init__.py
+++ mmrazor/models/architectures/components/backbones/__init__.py
@@ -1,10 +1,4 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .bignas_mobilenet import BigNASMobileNet
-from .darts_backbone import DartsBackbone
-from .searchable_mobilenet import SearchableMobileNet
-from .searchable_shufflenet_v2 import SearchableShuffleNetV2
 
-__all__ = [
-    'DartsBackbone', 'SearchableShuffleNetV2', 'SearchableMobileNet',
-    'BigNASMobileNet'
-]
+__all__ = ['BigNASMobileNet']
